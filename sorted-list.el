dde23e959a483a834b3686ef0374a3c506bc8bca
Boris Glavic <lordpretzel@gmail.com>
2022-04-24 13:38:17 -0500

    first commit with implementation

--- sorted-list.el
+++ sorted-list.el
@@ -25,18 +25,153 @@
 
 ;;; Commentary:
 
-;;
+;; Datastructure that wraps a sorted Lisp list by complementing it with an AVL
+;; tree which indexes the cons cells of the list. This allows O(log n) lookup,
+;; insertion and deletion for a regular elisp list.
 
 ;;; Code:
 
 ;; ********************************************************************************
 ;; IMPORTS
+(require 'avl-tree)
+(require 'cl-lib)
 
 ;; ********************************************************************************
-;; CUSTOM
+;; DATA STRUCTURES
+(cl-defstruct (sorted-list-
+               (:constructor nil)
+               (:constructor sorted-list--create
+                             (&optional lst
+                                        cmpfn
+                              &aux (tree (sorted-list--avl-index-list lst cmpfn)))))
+  "A list backed up with an avl tree for fast search."
+  (lst nil :type list :documentation "The user-facing list.")
+  (tree nil :type avl-tree- :documentation "The avl-tree indexing cons cells of the list.")
+  (cmpfn 'string< :type function :documentation "The comparison function for sorting."))
 
 ;; ********************************************************************************
 ;; FUNCTIONS
+(defun sorted-list--create-tree-cmp-fn (cmpfn)
+  "Create a comparison function for avl-tree backed lists from CMPFN for elements."
+  `(lambda (l r)
+     (let ((lv (car l))
+          (rv (car r)))
+      (funcall ',cmpfn lv rv))))
+
+(defun sorted-list--avl-next-smaller (tree data)
+  "Return the largest element in avl TREE which is smaller than DATA if it exists.
+
+Matching uses the comparison function previously specified in
+`avl-tree-create' when TREE was created. If there is no such
+element in the tree, nil is returned."
+  (let* ((node (avl-tree--root tree))
+         (path nil)
+	     (compare-function (avl-tree--cmpfun tree)))
+    (catch 'found
+      ;; go to leaf for not DATA if it exists
+      (while node
+	    (cond
+	     ((funcall compare-function data (avl-tree--node-data node))
+          (push (list node 'left) path)
+	      (setq node (avl-tree--node-left node)))
+	     ((funcall compare-function (avl-tree--node-data node) data)
+          (push (list node 'right) path)
+	      (setq node (avl-tree--node-right node)))
+	     (t
+          (if (avl-tree--node-left node)
+              ;; if have a left child, then take left branch, then right branches
+              (let ((n (avl-tree--node-left node)))
+                (while (avl-tree--node-right n)
+                  (setq n (avl-tree--node-right n)))
+                (throw 'found (avl-tree--node-data n)))
+            (setf node nil)))))
+      ;; either DATA does not exist in tree or it is a leaf node, find next smaller in parent
+      (cl-loop
+       for (p dir) in path
+       do
+       (when (equal dir 'right)
+         (throw 'found (avl-tree--node-data p))))
+      (throw 'found nil))))
+
+(defun sorted-list--avl-index-list (l cmpfn)
+  "Create an AVL tree that indexes the cells of list L sorted according to CMPFN."
+  (let ((tree (avl-tree-create (sorted-list--create-tree-cmp-fn cmpfn)))
+        (el l))
+    (sort l cmpfn)
+    (while el
+      (avl-tree-enter tree el)
+      (setq el (cdr el)))
+    tree))
+
+;;;###autoload
+(defun sorted-list-member-p (sl data)
+  "Does DATA exist in sorted list SL."
+  (let* ((tr (sorted-list--tree sl))
+         (el (avl-tree-member tr (cons data nil))))
+    (if el
+        (car el)
+      nil)))
+
+(defalias 'sorted-list-create #'sorted-list--create
+  "Create a sorted list. If LST is provided, then sort list to create the sorted list.
+CMPFN is a function which takes two arguments, A and B,
+and returns non-nil if A is less than B, and nil otherwise.")
+
+(defalias 'sorted-list-list #'sorted-list--lst
+  "Get the elisp list version of a sorted list.")
+
+(defun sorted-list--equals (smallerfn a b)
+  "Given a smaller-then function SMALLERFN, determine wether A = B."
+  (not (or (funcall smallerfn a b)
+           (funcall smallerfn b a))))
+
+(defun sorted-list-insert (sl tuple)
+  "Insert TUPLE into sorted list SL."
+  (let* ((tr (sorted-list--tree sl))
+         (tcell (cons tuple nil))
+         (el (avl-tree-member tr tcell))
+         (cmp (sorted-list--cmpfn sl)))
+    ;; if we already have element, append after last copy of element
+    (if el
+        ;; append after last item equal to tuple: insert a (a a a b b) -> (a a a a b b)
+        (let ((cs el))
+          (while (and (cdr cs) (sorted-list--equals cmp (cadr cs) tuple))
+            (setf cs (cdr cs)))
+          ;; insert after cs
+          (setcdr cs (cons
+                      tuple
+                      (cdr cs))))
+      ;; find next smaller element to insert after
+      (let ((prior-cs (sorted-list--avl-next-smaller tr tcell)))
+        (if prior-cs
+            ;; next smaller element exists
+            (let ((newcell (cons
+                            tuple
+                            (cdr prior-cs))))
+              (setcdr prior-cs newcell)
+              (avl-tree-enter tr newcell))
+            ;; no smaller element, create new head
+        (let* ((oldhead (sorted-list--lst sl))
+               (newhead (cons tuple oldhead)))
+          (setf (sorted-list--lst sl) newhead)
+          (avl-tree-enter tr newhead)))))))
+
+(defun sorted-list-delete (sl tuple)
+  "Delete first instance of TUPLE in sorted list SL."
+  (let* ((tr (sorted-list--tree sl))
+         (tcell (cons tuple nil))
+         (el (avl-tree-member tr tcell))
+         (cmp (sorted-list--cmpfn sl)))
+    (avl-tree-delete tr (cons tuple nil))
+    (if (and (cdr el) (sorted-list--equals cmp (cadr el) tuple))
+        ;; if not last duplicate of TUPLE, then add back to avl-tree
+        (avl-tree-enter tr (cdr el)))
+      ;; delete el
+      (let ((prior-cs (sorted-list--avl-next-smaller tr tcell)))
+        (if prior-cs
+            (setcdr prior-cs (cddr prior-cs))
+          ;; el is first element: delete list head
+          (setf (sorted-list--lst sl) (cdr (sorted-list--lst sl)))))))
 
 (provide 'sorted-list)
 ;;; sorted-list.el ends here
