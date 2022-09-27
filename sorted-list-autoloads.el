a87bd866f4ff8cea7777166cd825d56124c137d5
Boris Glavic <lordpretzel@gmail.com>
2022-09-27 13:13:07 -0500

    autoloads

--- sorted-list-autoloads.el
+++ sorted-list-autoloads.el
@@ -30,6 +30,29 @@ Does DATA exist in sorted list SL.
 (register-definition-prefixes "sorted-list" '("sorted-list-"))
 
 ;;;***
+
+
+;;; Generated autoloads from sorted-list.el
+
+(autoload 'sorted-list-member-p "sorted-list" "\
+Does DATA exist in sorted list SL.
+
+(fn SL DATA)")
+(defalias 'sorted-list-create #'sorted-list--create "\
+Create a sorted list. If LST is provided, then sort list to create the sorted list.
+CMPFN is a function which takes two arguments, A and B,
+and returns non-nil if A is less than B, and nil otherwise.")
+(defalias 'sorted-list-list #'sorted-list--lst "\
+Get the elisp list version of a sorted list.")
+(autoload 'sorted-list-insert "sorted-list" "\
+Insert TUPLE into sorted list SL.
+
+(fn SL TUPLE)")
+(autoload 'sorted-list-delete "sorted-list" "\
+Delete first instance of TUPLE in sorted list SL.
+
+(fn SL TUPLE)")
+(register-definition-prefixes "sorted-list" '("sorted-list--"))
 
 ;; Local Variables:
 ;; version-control: never
